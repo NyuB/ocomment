32b3fa2b09ab7563dfea25dbfe4145f93c3fead4
NyuB <brice.decaestecker@gmx.fr>
2023-08-11 13:43:49 +0200

    Add usage examples

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# ocomment
+# Ocomment
 File portion checksum enforcer aimed to reduce comments entropy
+
+## Usage
+
+```bash
+./ocomment <ocomment_settings_file> filepath_1 filepath_2 ... filepath_n
+```
+
+Where **ocomment_settings_file** should follow this structure:
+
+```clojure
+((promotion_dir test/resources/.promote)
+   (includes (
+    (.java ((start_marker "// <#") (end_marker "// #>")))
+    (.py ((start_marker "# ocomment start") (end_marker "# ocomment end")))
+    (...)
+    (.<file_extension> ((start_marker "start?") (end_marker "end?"))))))
+```
+
+The **promotion_dir** field indicate the folder where to generate corrected files when invalid checksum are detected.
+
+The **includes** section is a list of association between extensions and markers. All files passed as argument to the executable matching one of these extensions will be validated using the corresponding markers.
+
+### Examples :
+
+#### Validate one file
+
+```bash
+./ocomment .ocomment src/main/com/app/Hello.java
+```
+
+#### Validate all .py files in a 'src' folder
+
+```bash
+./ocomment .ocomment src/**/*.py
+```
+
+NB : If no .py extension markers are configured in the .ocomment this would have no effect
+
+#### Validate all files modified since last commit
+
+```bash
+./ocomment .ocomment $(git diff --name-only)
+```
+
+#### Validate all files modified between the last two commits in a given folder
+
+```bash
+./ocomment .ocomment $(git diff --name-only HEAD HEAD~ src/)
+```
--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,44 @@
+let help =
+  {|
+  Usage: ocomment <settings_file> filepath_1 filepath_2 ... filepath_n
+    Where <settings_file> should follow this structure:
+      ((promotion_dir test/resources/.promote)
+      (includes (
+      (.java ((start_marker "// <#") (end_marker "// #>")))
+      (.py ((start_marker "# ocomment start") (end_marker "# ocomment end")))
+      (...)
+      (.<file_extension> ((start_marker "start?") (end_marker "end?"))))))
+  
+  Examples:
+  > Validate one file
+  ./ocomment .ocomment src/main/com/app/Hello.java
+  
+  > Validate all .py files in a 'src' folder
+  ./ocomment .ocomment src/**/*.py
+  NB : If no .py extension markers are configured in the .ocomment this would have no effect
+  
+  > Validate all files modified since last commit
+  ./ocomment .ocomment $(git diff --name-only)
+  
+  > Validate all files modified between the last two commits in a given folder
+  ./ocomment .ocomment $(git diff --name-only HEAD HEAD~ src/)
+|}
+;;
+
 let () =
   let args = Array.sub Sys.argv 1 (Array.length Sys.argv - 1) in
-  let settings_file = args.(0) in
-  let files = Array.sub args 1 (Array.length args - 1) |> Array.to_list in
-  match Ocomment.App.check_with_promotion ~settings_file ~files with
-  | Ok n -> exit n
-  | Error e -> failwith e
+  if Array.length args < 1
+  then (
+    print_endline help;
+    exit (-1))
+  else if args.(0) = "--help" || args.(0) = "-h"
+  then (
+    print_endline help;
+    exit 0)
+  else (
+    let settings_file = args.(0) in
+    let files = Array.sub args 1 (Array.length args - 1) |> Array.to_list in
+    match Ocomment.App.check_with_promotion ~settings_file ~files with
+    | Ok n -> exit n
+    | Error e -> failwith e)
 ;;
